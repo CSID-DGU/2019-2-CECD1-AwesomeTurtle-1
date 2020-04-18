434f373f4f0ff3ce541c7266a5125498ca553c92
Ju-Yeon Park <pjuyeon25@gmail.com>
2020-04-19 02:40:12 +0900

    get text and parse to csv

--- src/patentInsight/routes/index.js
+++ src/patentInsight/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var multer = require('multer');
+var preprocessor = require('../module/preprocess')
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('main.html');
 });
+
+//파일입력
+router.post('/file', multer({ dest: '../../data/'}).single('input_file'), async function(req,res){
+
+  var csvdata = await preprocessor.readToCsv(req.file.path)
+  res.render('output.html', {csvdata} );
+
+});
+
 
 module.exports = router;
